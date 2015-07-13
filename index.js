1dd53eabe1d56459e4d111a9d782632c5369aee0
mkotelnikov <mikhail.kotelnikov@ubimix.com>
2015-07-14 01:06:33 +0200

    Externalize URL management (app state / URL mapping) in a separate class.

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 import Application from './lib/Application';
 import AppModule from './lib/AppModule';
+import AppNavigation from './lib/AppNavigation';
+
 export default {
     Application,
-    AppModule
+    AppModule,
+    AppNavigation
 }
