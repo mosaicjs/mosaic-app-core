b4a3d37e23cbd0da67a99be4b333fa98e13354cf
mkotelnikov <mikhail.kotelnikov@ubimix.com>
2015-07-27 12:23:50 +0200

    Add AppUrlNavigation

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ import AppRouter from './lib/AppRouter';
 import Application from './lib/Application';
 import AppModule from './lib/AppModule';
 import AppNavigation from './lib/AppNavigation';
+import AppUrlNavigation from './lib/AppUrlNavigation';
 
 export default {
     AppRouter,
     Application,
     AppModule,
-    AppNavigation
+    AppNavigation,
+    AppUrlNavigation
 }
